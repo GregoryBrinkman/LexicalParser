261717791ffdd855d229a111bf9f207d769b9547
Gregory Brinkman <GregoryLucasBrinkman@gmail.com>
2018-03-01 22:22:14 -0600

    Added standard format

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-let text = getGrammar();
-console.log(text);
+let num = 6;
+let currentLine = 1;
+let currentChar = 1;
+main();
+
+function main() {
+  let text = getGrammar();
+  //Do some work here
 
 
-
-
-
-
-
-
-
-function getGrammar(){
-  let fs = require('fs');
-  let path = require('path');
-
-  return fs.readFileSync(path.join(__dirname, '/grammar.txt')).toString();
+  //token loop
+  next();
+  while ( kind() != "end-of-text" ) {
+    console.log(value() + ", " + kind() + ", " + position());
+    next();
+  }
+  console.log(kind());
 }
 
 function next() {
@@ -22,17 +23,36 @@ function next() {
 }
 
 function kind() {
-  //returns the knid of lexeme that was read
-  return 'kind';
+  //returns the kind of lexeme that was read
+  let kind = '';
+  kind = 'kind';
+  if(num <= 0) { kind = 'end-of-text'; }
+
+  num--;
+  return kind;
 }
 
 function value() {
   //returns the value of the lexeme (if it is an "ID" or "NUM")
-  return 'value';
+
+  let value = '';
+
+  value = 'ID';
+  value = 'NUM';
+
+  return value;
+
 }
 
 function position() {
   //returns the position of the lexeme that was just read
-  return 'position';
+  let position = currentLine + ":" + currentChar;
+  return position;
 }
 
+function getGrammar(){
+  let fs = require('fs');
+  let path = require('path');
+
+  return fs.readFileSync(path.join(__dirname, '/grammar.txt')).toString();
+}
